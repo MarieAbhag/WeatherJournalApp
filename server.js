f4b6874014d2da31adc62a2ea62911c5f192ff4c
Amir Tosson <marieabhag282@gmail.com>
2021-09-22 12:34:17 +0200

    Try with videos before implementation

--- server.js
+++ server.js
@@ -1,19 +1,52 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+// Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser')
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
+app.get('/', function (req, res) {
+    res.send('hello world');
+  })
 // Setup Server
+
+// Set up and Spin up the server
+const port = 8080;
+const server = app.listen(port, () => {
+    console.log(server.address())
+    console.log(`server is listening on port: ${port}`); // Callback to debug
+});
+
+
+
+// Create JS object
+const appData = {
+    date:'sss'
+}
+// Respond with JS object when a GET request is made to the homepage
+app.get('/all', function (req, res) {
+  res.send(appData)
+})
+
+const data = [];
+app.post('/flavor', addFlavor);
+
+function addFlavor (req, res) {
+  console.log(req.body.date);
+};
--- website/app.js
+++ website/app.js
@@ -3,3 +3,27 @@
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', DoSomething);
+
+
+function DoSomething(){
+    console.log("HERE");
+    postData('/all', "Here" );
+}
+
+
+
+const postData = async(url = '', data) => {
+    const response = await fetch(url);
+
+    try {
+        const newData = await response.json();
+        console.log(newData.date);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+postData('/all', "Here" );
